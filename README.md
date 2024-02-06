ec876b2a383038b24409ac2b5904279595842848
Shiwen Han <tshogx@outlook.com>
2024-02-06 18:00:03 -0500

    first version

---
+++ README.md
@@ -0,0 +1,7 @@
+# MD COPY
+
+A Firefox/Chrome extension allowing you to copy a markdown style link `[title](url)` to clipboard via context menu. 
+
+Right click on selected text to copy `selection [title](url)`.
+
+Thanks to [this](https://github.com/marekjedlinski/webext-copy-title-url)
---
+++ background.js
@@ -0,0 +1,39 @@
+const REQUEST_TITLE = "md-copy-page";
+const REQUEST_SELECTION = "md-copy-selection";
+
+/* This extension creates two context menu items: one for the default case,
+    and one to use when some text is selected on a web page. Only one of the
+    two items is shown at a time, otherwise Firefox puts them in a submenu,
+    which requires additional clicks to navigate.
+*/
+
+// support Firefox and Chrome
+if (!browser) {
+  var browser = chrome;
+}
+
+browser.contextMenus.create({
+  id: REQUEST_TITLE,
+  title: "copy page",
+  contexts: ["page"],
+  // onclick: handleCopyRequest,
+});
+browser.contextMenus.create({
+  id: REQUEST_SELECTION,
+  title: "copy selection",
+  contexts: ["selection"],
+  // onclick: handleCopyRequest,
+});
+
+// Add event listener for menu item clicks
+browser.contextMenus.onClicked.addListener(handleCopyRequest);
+
+function handleCopyRequest(info, tab) {
+  // check in which context the command was clicked
+  const useSelection = info.menuItemId === REQUEST_SELECTION;
+  chrome.tabs.sendMessage(tab.id, {
+    tabUrl: tab.url,
+    tabTitle: tab.title,
+    useSelection: useSelection,
+  });
+}
